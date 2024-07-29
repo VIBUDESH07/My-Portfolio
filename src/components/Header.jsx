2348251c549efee60bdd4a9735f7fa6c504a0d5b
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-07-29 18:01:03 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-
+import './styles/heade.css'
 const Header = () => {
   return (
-    <header className="bg-black text-orange-500 p-4">
+    <header className="bg-black text-orange-500 p-4 shadow header-box">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-3xl font-bold">My Portfolio</h1>
+        <h1 className="text-3xl font-bold text-shadow"> 
+          <span className='blink'>MY PORTFOLIO</span></h1>
         <nav>
-          <a href="#about" className="px-3 transition duration-300 transform hover:text-gray-400 hover:scale-110">About</a>
-          <a href="#projects" className="px-3 transition duration-300 transform hover:text-gray-400 hover:scale-110">Projects</a>
-          <a href="#contact" className="px-3 transition duration-300 transform hover:text-gray-400 hover:scale-110">Contact</a>
+          <a href="#about" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">About</a>
+          <a href="#projects" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">Projects</a>
+          <a href="#contact" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">Contact</a>
         </nav>
-        <button className="bg-orange-500 text-black px-4 py-2 rounded-full transition duration-300 transform hover:bg-orange-600 hover:scale-110">
+        <button className="bg-orange-500 text-black px-4 py-2 rounded-full transition duration-300 transform hover:bg-orange-600 text-shadow">
           Email
         </button>
       </div>
     </header>
   );
 }
-
 export default Header;
