79906c785d57872246e5e30ad722e703c442593a
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-08-08 21:13:29 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
-import './styles/heade.css'; // Ensure this CSS file is imported
+import './styles/heade.css';
 
 const Header = () => {
+  const handleSmoothScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <header className="bg-black text-orange-500 p-4 shadow header-box">
       <div className="container mx-auto flex justify-between items-center">
@@ -9,11 +17,26 @@ const Header = () => {
           <span className='portfolio-blink'>MY PORTFOLIO</span>
         </h1>
         <nav>
-          <a href="#about" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">About</a>
-          <a href="#projects" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">Projects</a>
-          <a href="#skills" className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">Skills</a>
-          </nav>
-        <a href="#contact">
+          <a 
+            href="#about" 
+            onClick={(e) => handleSmoothScroll(e, 'about')} 
+            className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">
+            About
+          </a>
+          <a 
+            href="#projects" 
+            onClick={(e) => handleSmoothScroll(e, 'projects')} 
+            className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">
+            Projects
+          </a>
+          <a 
+            href="#skills" 
+            onClick={(e) => handleSmoothScroll(e, 'skills')} 
+            className="mx-10 px-3 py-2 transition duration-300 transform hover:bg-orange-500 hover:text-black hover:rounded-md text-shadow">
+            Skills
+          </a>
+        </nav>
+        <a href="#contact" onClick={(e) => handleSmoothScroll(e, 'contact')}>
           <button className="bg-orange-500 text-black px-4 py-2 rounded-full transition duration-300 transform hover:bg-orange-600 text-shadow">
             Contact
           </button>
