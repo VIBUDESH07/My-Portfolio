b747147604d8f51f27f71703dc90165276d8bd07
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-09-25 12:09:41 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,32 +3,32 @@ import { InformationCircleIcon, BriefcaseIcon, DeviceMobileIcon, MailIcon } from
 
 const Navbar = () => {
   return (
-    <nav className="bg-white py-4 text-black mx-[36rem] my-[0.5rem]">
+    <nav className="fixed top-2 left-1/2 transform -translate-x-1/2 bg-white py-3 px-6 text-black rounded-full shadow-lg z-50">
       <div className="container mx-auto flex justify-around items-center">
         <ul className="flex space-x-10">
           <li className="m-2">
-            <a href="#about" className="flex items-center space-x-2 hover:text-gray-200">
+            <a href="#about" className="flex items-center space-x-2 hover:text-gray-400 transition">
               <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
                 <InformationCircleIcon className="w-6 h-6 text-white" />
               </div>
             </a>
           </li>
           <li className="m-2">
-            <a href="#projects" className="flex items-center space-x-2 hover:text-gray-200">
+            <a href="#projects" className="flex items-center space-x-2 hover:text-gray-400 transition">
               <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
                 <BriefcaseIcon className="w-6 h-6 text-white" />
               </div>
             </a>
           </li>
           <li className="m-2">
-            <a href="#skills" className="flex items-center space-x-2 hover:text-gray-200">
+            <a href="#skills" className="flex items-center space-x-2 hover:text-gray-400 transition">
               <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
                 <DeviceMobileIcon className="w-6 h-6 text-white" />
               </div>
             </a>
           </li>
           <li className="m-2">
-            <a href="#contact" className="flex items-center space-x-2 hover:text-gray-200">
+            <a href="#contact" className="flex items-center space-x-2 hover:text-gray-400 transition">
               <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
                 <MailIcon className="w-6 h-6 text-white" />
               </div>
