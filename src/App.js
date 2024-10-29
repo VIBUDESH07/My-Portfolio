8c3b594fd9265d432a451dd76013c6b33026a242
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2024-10-29 12:08:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Education from './components/Education';
+import Achievement from './components/Achievement';
 
 function App() {
   return (
     <div className="App relative">
       <Navbar/>
       <About/>
-
+      <Education/>
+      <Achievement/>
       </div>
   );
 }
