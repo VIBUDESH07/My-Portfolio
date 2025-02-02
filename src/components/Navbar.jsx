e1b9401bfa8386a24985e861ac411072c30cc9fb
VIBUDESH07 <125853344+VIBUDESH07@users.noreply.github.com>
2025-02-02 13:20:10 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { InformationCircleIcon, BriefcaseIcon, DeviceMobileIcon, MailIcon } from '@heroicons/react/solid';
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <nav className="bg-white py-4 text-black mx-[36rem] my-[0.5rem]">
+      <div className="container mx-auto flex justify-around items-center">
+        <ul className="flex space-x-10">
+          <li className="m-2">
+            <a href="#about" className="flex items-center space-x-2 hover:text-gray-200">
+              <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
+                <InformationCircleIcon className="w-6 h-6 text-white" />
+              </div>
+            </a>
+          </li>
+          <li className="m-2">
+            <a href="#projects" className="flex items-center space-x-2 hover:text-gray-200">
+              <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
+                <BriefcaseIcon className="w-6 h-6 text-white" />
+              </div>
+            </a>
+          </li>
+          <li className="m-2">
+            <a href="#skills" className="flex items-center space-x-2 hover:text-gray-200">
+              <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
+                <DeviceMobileIcon className="w-6 h-6 text-white" />
+              </div>
+            </a>
+          </li>
+          <li className="m-2">
+            <a href="#contact" className="flex items-center space-x-2 hover:text-gray-200">
+              <div className="w-8 h-8 bg-gradient-to-r from-purple-500 to-blue-500 p-1 rounded-full">
+                <MailIcon className="w-6 h-6 text-white" />
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
